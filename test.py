3a7ed9c389f1025ca7e10c183d08abff9cddc918
Salae_Aletta <49242261+Sakae28@users.noreply.github.com>
2024-08-12 16:39:06 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+
+# for test
+from XHSScraper.scraper import XHSScraper
+from XHSScraper.config import KEYWORD, DEFAULT_CRAWL_TIMES, DEFAULT_WAIT_TIME, LOG_FORMAT, LOG_LEVEL, LOGIN_URL, SEARCH_URL_TEMPLATE
+
+xhs = XHSScraper()
+xhs.get_data(KEYWORD)
+
+
+
+
+
